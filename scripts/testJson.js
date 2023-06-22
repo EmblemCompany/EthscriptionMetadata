61e4100a2c3e34d21d2f6b3b3e55dbd875225586
shannon code <genecyber@gmail.com>
2023-06-21 19:57:58 -0700

    build and test

--- scripts/testJson.js
+++ scripts/testJson.js
@@ -1,20 +1,30 @@
-const fs = require('fs');
+const fs = require('fs')
+
+if (process.argv[2] == "test") {
+    let baseFolder = process.argv[3]
+    const files = fs.readdirSync(baseFolder);
+    files.forEach((filePath, index) => {
+        if (!isValid(baseFolder + filePath)) {
+            console.log("FAILED")
+            process.exit(1)
+        }
+        console.log('testing file: ' + filePath, index)
+        if (files.length === index + 1) {
+            console.log('done')
+        }
+    })
+}
 
 function isValid(filePath) {
     try {
-        const fileContent = fs.readFileSync(filePath, 'utf8');
-        const jsonObject = JSON.parse(fileContent);
-
-        // Here you can add additional validation logic
-        // if required, for now it only checks if it's valid JSON
-
-        // If the file is an array, return it, else wrap it in an array
-        return Array.isArray(jsonObject) ? jsonObject : [jsonObject];
+        const fileContent = fs.readFileSync(filePath, 'utf8')
+        const jsonObject = JSON.parse(fileContent)
+        return Array.isArray(jsonObject) ? jsonObject : [jsonObject]
 
     } catch (error) {
-        console.error(`Error parsing file ${filePath}: ${error.message}`);
-        return null;
+        console.error(`Error parsing file ${filePath}: ${error.message}`)
+        return null
     }
 }
 
-module.exports = { isValid };
+module.exports = { isValid }
